de39c1fca028a4c7206b71c199d953e2f087f1ec
Yuvraj D <67145962+thatonearchguy@users.noreply.github.com>
2024-02-16 22:45:17 +0000

    Added rotation and simplified pathfinding interface

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <SPI.h>
 #include <Adafruit_TCS34725.h>
 #include <DFRobot_URM09.h>
+#include <TimerOne.h>
 
 #define ABS_FORWARD 2
 #define ABS_BACKWARD 4
@@ -29,7 +30,7 @@ int rightJctPin = 3;
 uint8_t avgMotorSpeed = 150; 
 
 float Kp = 1;
-float Ki = 0;
+float Ki = 0.3;
 float Kd = 0.6;
 
 float prop;
@@ -37,6 +38,8 @@ float integ;
 float deriv;
 int lastError;
 int correction;
+
+bool insideEdge; //used to tell PID which edge to follow (inside or outside)
 
 //Colour sensor
 int idealLightValue = 350;
@@ -48,16 +51,26 @@ bool rightJctDetect = false;
 bool tJctDetect = false;
 
 //Pathfinding
-uint8_t currDirect;
+uint8_t currDirect = ABS_FORWARD; //default is forwards
 uint8_t currNode;
 
 //Ranging Sensor
 int block_threshold_mm = 50; //adjust for chassis geometry
 
-bool verify_turn(uint8_t new_direc, uint8_t currNode, uint8_t nextNode, uint8_t currDirect);
+//IMU
+const int integration_interval = 3000; //microseconds - 3ms, tune depending on integration accuracy.
+float yawAngle = 0;
+float yawData;
+bool turnReady = false;
+int newDirect;
+
+bool verify_turn();
 void jct_int_handler();
 void make_turn(uint8_t new_direc);
-int get_next_turn(int currDirect);
+void get_next_turn(int newDirection);
+void calculate_pid(int valAvg, int *correct);
+void calculate_angle(int yawData);
+float get_rotation_data();
 
 void setup() {
 
@@ -89,6 +102,14 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(rightJctPin), jct_int_handler, RISING);
   
   correction = 0;
+
+  Timer1.initialize(integration_interval);
+
+}
+
+void calculate_angle()
+{
+  yawAngle += 0.5 * (yawData + get_rotation_data()) * (integration_interval / 1e6);
 }
 
 void jct_int_handler()
@@ -96,11 +117,9 @@ void jct_int_handler()
   leftJctDetect = digitalRead(leftJctPin);
   rightJctDetect = digitalRead(rightJctPin);
   tJctDetect = leftJctDetect & rightJctDetect;
-  /*
-  //check if junction directions are what we expect for the current node
-  int new_direc = get_next_turn(currDirect);
-  make_turn(new_direc);
-  */
+  
+  turnReady = true;
+
 }
 
 
@@ -115,11 +134,23 @@ void calculate_pid(int valAvg, int* correct)
   lastError = error;
 }
 
-int get_next_turn(int currDirect)
+
+void get_next_turn(int newDirection)
 {
-  //connect to pathfinding logic
-  //and check if junction directions are what we expect for the current node
-  //talk to motors and get it to swivel the bot 90 degrees in the desired direction
+  //connect to pathfinding logic,
+  //get absolute value of next turn
+  if (newDirection++ > 4) newDirection = 1;
+}
+
+
+float get_rotation_data()
+{
+  float x, y, z;
+  if(IMU.gyroscopeAvailable())
+  {
+    IMU.readGyroscope(x, y, z);
+  }
+  return z;
 }
 
 
@@ -135,10 +166,41 @@ uint16_t get_colour_data()
   return int(valSum/numAvgSamples);
 }
 
-void set_motor_speed(int correction)
+void pid_motor_regulate(int correction)
 {
   leftWheel->setSpeed(constrain((avgMotorSpeed - correction), 0, 255));
   rightWheel->setSpeed(constrain((avgMotorSpeed + correction), 0, 255));
+}
+
+
+void make_turn(uint8_t newDirect)
+{
+  yawAngle = 0;
+  Timer1.attachInterrupt(calculate_angle); //start numerically integrating rotation
+  int desiredAngle = (currDirect - newDirect) * (PI/2); //gyro is in radians
+  
+  leftWheel->setSpeed(avgMotorSpeed);
+  rightWheel->setSpeed(avgMotorSpeed);
+
+  if(desiredAngle > 0)
+  {
+    leftWheel->run(FORWARD);
+    rightWheel->run(BACKWARD);
+  }
+  else
+  {
+    leftWheel->run(BACKWARD);
+    rightWheel->run(FORWARD);
+  }
+
+  //account for negative (counterclockwise) case
+  while(abs(yawAngle) < abs(desiredAngle)) //wait
+
+  leftWheel->fullOff();
+  rightWheel->fullOff();
+
+  currDirect = newDirect;
+
 }
 
 void loop(void) {
@@ -146,22 +208,23 @@ void loop(void) {
   rightWheel->run(FORWARD);
   uint16_t valAvg = get_colour_data();
   calculate_pid(valAvg, &correction);
-  set_motor_speed(correction);
+  pid_motor_regulate(correction);
 
-  if(leftJctDetect & rightJctDetect)
+  Serial.print("Current angle: ");
+  Serial.println(yawAngle);
+
+  if(turnReady)
   {
-    Serial.println("T/Cross Junction Detected");
-  }
+    /*
+    check numerical integration of accelerometer x/y and ensure
+    total travelled distance is within expected value (compare against weight)
 
-  else if(rightJctDetect)
-  {
-    Serial.println("Right Junction Detected!");
+    */
+    leftWheel->fullOff();
+    rightWheel->fullOff();
+    get_next_turn(newDirect);
+    make_turn(newDirect);
+    turnReady = !turnReady;
   }
-
-  else if(leftJctDetect)
-  {
-    Serial.println("Left Junction Detected!");
-  }
-
 
 }
